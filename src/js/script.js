97a1b51e018152f3c615451f294fd6fe23070dcc
Igor Kovalis <igor_kovalis@rambler.ru>
2023-10-11 16:48:56 +0300

    add full page

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,37 @@
-// import ready from "../js/utils/documentReady.js";
+import ready from "../js/utils/documentReady.js";
+import { selectWrap } from "./common/select.js";
+import noUiSlider from "nouislider";
 // import getScrollSize from "../js/utils/getScrollSize.js";
 
-// ready(function () {
-//   console.log("DOM героически построен!");
-//   // Добавление кастомного свойства с системной шириной скролла
-//   document.documentElement.style.setProperty("--css-scroll-size", `${getScrollSize()}px`);
-// });
+ready(function () {
+  //custom selets
+  selectWrap();
 
-// import $ from "jquery"; // Перед использованием установить как зависимость
-// $(function () {
-//   console.log("jQuery героически сработал!");
-// });
+  //range
+  let range = document.querySelectorAll('.range');
+
+  if(range){
+      range.forEach((item) => {
+        const startValue = 75;
+          noUiSlider.create(item, {
+              start: startValue,
+              step: 1,
+              range: {
+                  'min': 0,
+                  'max': 100
+              }
+          });
+
+          const rangeText = item.closest(".order-block").querySelector(".js-range__value");
+          const rangeValue = item.closest(".order-block").querySelector(".js-range__value-input");
+          if(rangeText){
+            rangeText.innerText = startValue;
+
+            item.noUiSlider.on('update', function (values, handle) {
+              rangeText.innerText = Math.round(values[handle]);
+              rangeValue.value = Math.round(values[handle]);
+            });
+          }
+      });
+  }
+});
